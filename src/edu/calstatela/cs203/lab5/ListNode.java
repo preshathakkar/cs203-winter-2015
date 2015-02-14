844d05085dc65f4261377a1db8f954e660f7e7b5
jtran-csula <jtran-csula@users.noreply.github.com>
2015-02-14 10:37:40 -0800

    Create ListNode.java

---
+++ src/edu/calstatela/cs203/lab5/ListNode.java
@@ -0,0 +1,16 @@
+package edu.calstatela.cs203.lab5;
+
+public class ListNode {
+	Object object;
+	ListNode next;
+	
+	ListNode(Object object) {
+		this();
+		this.object = object;
+	}
+	
+	ListNode() {
+		next = null;
+	}
+	
+}
