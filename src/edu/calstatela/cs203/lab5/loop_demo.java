67f0c601af76eb9c5d6d36981be1548a10789a01
jtran-csula <jtran-csula@users.noreply.github.com>
2015-02-14 10:41:25 -0800

    Create loop_demo.java

---
+++ src/edu/calstatela/cs203/lab5/loop_demo.java
@@ -0,0 +1,16 @@
+package edu.calstatela.cs203.lab5;
+
+public class loop_demo {
+	public static void main(String[] args) {
+		f(5);
+	}
+	
+	// System.out.println("john");
+	static void f(int n) {
+		if (n == 0) {
+			return;
+		}
+		System.out.println(n + " john");
+		f(n-1);
+	}
+}
