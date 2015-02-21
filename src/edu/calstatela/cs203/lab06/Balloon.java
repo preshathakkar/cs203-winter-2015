e7d57c52555c3e6ac8316f96a581122e18d92c1e
John Tran <jtran-csula@pcwerk.com>
2015-02-21 10:28:02 -0800

    decompose into separate object

--- src/edu/calstatela/cs203/lab06/Balloon.java
+++ src/edu/calstatela/cs203/lab06/Balloon.java
@@ -1,44 +1,52 @@
 package edu.calstatela.cs203.lab06;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.UUID;
 
-public class Balloon {
-	int id;
-	private static int idcount = 0;
-	
-	Balloon() {
-		id = idcount++;
+class IdCollection {
+	public String getId() {
+		boolean done = false;
+		String id = "";
+		do {
+			String key = UUID.randomUUID().toString();
+			boolean found = false;
+			for (String i : ids) {
+				if (i.equals(key)) {
+					found = true;
+				}
+			}
+			
+			if (!found) {
+				id = key;
+				ids.add(key);
+			}
+		} while (!done);
+		
+		return id;
 	}
-	
+	private java.util.ArrayList<String> ids = new java.util.ArrayList<String>();
+}
+
+public class Balloon {
+	String id;
+	private static IdCollection idCollection = new IdCollection();
+
+	Balloon() {
+		id = idCollection.getId();
+	}
+
 	public String toString() {
 		return id + "";
 	}
-	
-	public static void main(String[]args ) {
+
+	public static void main(String[] args) {
 		java.util.ArrayList<Balloon> list = new java.util.ArrayList<Balloon>();
-		for (int i = 0; i < 5; i++) { 
+
+		for (int i = 0; i < 5; i++) {
 			list.add(new Balloon());
 		}
-		
+
 		for (Balloon b : list) {
 			System.out.println(b);
 		}
-		
-		Iterator<Balloon> it = list.iterator();
-		System.out.println(it.next());
-		System.out.println(it.next());
-		
-		Map<String,Balloon> map = new HashMap<String,Balloon>();
-		UUID uuid = UUID.randomUUID();
-		String key = uuid.toString();
-		Balloon value = new Balloon();
-		System.out.println("key = " + key);
-		System.out.println("value = " + value);
-		map.put(key, value);
-		Balloon b = map.get(key);
-		System.out.println("value = " + b);
 	}
 }
