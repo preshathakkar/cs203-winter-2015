3c2f01d91fb0fd310b5e0080e05efdfde37c6d62
John Tran <jtran-csula@pcwerk.com>
2015-03-04 02:59:23 -0800

    add stop words

--- src/edu/calstatela/cs203/lab07/Reader.java
+++ src/edu/calstatela/cs203/lab07/Reader.java
@@ -1,20 +1,24 @@
 package edu.calstatela.cs203.lab07;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class Reader {
 	
 	private static String[] stopWords = { "to", "the" };
+	private static Set<String> stopWordsSet = new HashSet<String>();
 	
 	private ArrayList<String> cleanUp(String[] words) {
 		ArrayList<String> cleanWords = new ArrayList<String>();
 		
 		for (String word : words) {
 			String t = word.replaceAll("\\.", "").replaceAll(";", "").replaceAll(":", "");
-			cleanWords.add(t);
+			if (!stopWordsSet.contains(t))
+				cleanWords.add(t);
 		}
 		
 		return cleanWords;
@@ -29,7 +33,11 @@ public class Reader {
 		lines[2] = scanner.nextLine();
 		
 		Map<String,Integer> table = new TreeMap<String,Integer>();
-	
+		
+		for (String word : stopWords) {
+			stopWordsSet.add(word);
+		}
+		
 		for (String line : lines) {
 			String[] words = line.split("\\s+");
 			for (String cleanWord : cleanUp(words)) {
